85f3163ac5b5d3b29965be0ce91732fc44328549
tumise <oladele.alade@gmail.com>
2024-02-18 20:55:02 +0000

    remove old path

---
+++ src/test/java/com/financial/timeseries/TimeSeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financial.timeseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeSeriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
